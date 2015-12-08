3527cf92e952041e7011a3e365ec4ae4880a4162
Jean D'Amore <jdamore@thoughtworks.com>
2015-12-08 16:27:14 +1000

    Fixed setting the state afer componentDidMount

--- src/app/card/index.js
+++ src/app/card/index.js
@@ -3,28 +3,24 @@ import styles from './index.scss'
 
 module.exports = React.createClass({
 
-  // constructor: function() {
-  //   this.state = { word: '', score: 0 };
-  // },
-
   getInitialState: function() {
-    return { word: 'xx', score: 0 };
+    console.log('getInitialState');
+    return { word: '', score: 0 };
   },
 
   componentDidMount: function() {
+    console.log('componentDidMount');
     /* should get the word and score from a webservices */
-    this.setState('a word', 10);
-  },
-
-  setState: function(word, score) {
-    this.state = { word: word, score: score };
+    this.setState( { word:'a word', score: 10} );
   },
 
   onClick: function(event) {
+    console.log('onClick');
     document.getElementById('card').classList.toggle('flick')
   },
 
   render: function() {
+    console.log('render');
   	return (
   		<div id='card' onClick={this.onClick} className={styles.container}>
   				<div className={styles.flicker}>
