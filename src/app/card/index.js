19d203ea9c04786476e1df9f3e5563b4bea631be
Jean D'Amore <jdamore@thoughtworks.com>
2016-01-09 22:26:48 +1000

    Slight refactor in card/index.js

--- src/app/card/index.js
+++ src/app/card/index.js
@@ -8,13 +8,15 @@ class Card extends React.Component {
 
   constructor() {
     super();
-    this.words = ['temps', 'cela', 'constitution', 'mais', 'encore'];
-    this.state = { word: '', score: 0 };
-    this.onClick = this._onClick.bind(this);
-    this.cardNode = this._cardNode.bind(this);
-    this.cardChildNode = this._cardChildNode.bind(this);
-    this.newWord = this._newWord.bind(this);
-    this._styles = styles;
+    this.words          = ['temps', 'cela', 'constitution', 'mais', 'encore'];
+    this.state          = { word: '', score: 0 };
+    this.onClick        = this._onClick.bind(this);
+    this.cardNode       = this._cardNode.bind(this);
+    this.cardChildNode  = this._cardChildNode.bind(this);
+    this.newWord        = this._newWord.bind(this);
+    this.flipCard       = this._flipCard.bind(this);
+    this.changeWord     = this._changeWord.bind(this);
+    this._styles        = styles;
   }
 
   componentDidMount() {
@@ -37,17 +39,23 @@ class Card extends React.Component {
   }
 
   _onClick() {
+    this.flipCard();
+    this.changeWord();
+  }
+
+  _changeWord() {
+    this.setState(function(previousState, currentProps) {
+      return (previousState.side==='front') ?
+        { side:'back', word:previousState.word, score: previousState.score }
+      :
+        { side:'front', word:this.newWord(), score: previousState.score };
+    });
+  }
+
+  _flipCard() {
     this.cardNode().classList.toggle(styles.flipped);
     this.cardChildNode('front').classList.toggle(styles.flipped);
     this.cardChildNode('back').classList.toggle(styles.flipped);
-    this.setState(function(previousState, currentProps) {
-      if(previousState.side==='front') {
-        return { side:'back', word:previousState.word, score: previousState.score };
-      }
-      else {
-        return { side:'front', word:this.newWord(), score: previousState.score };
-      }
-    });
   }
  
   _cardNode() {
