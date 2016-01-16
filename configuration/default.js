f69c453050fbfda289b7f65cff1278ddb0af7de2
Jean D'Amore <jdamore@thoughtworks.com>
2016-01-16 23:06:20 +1000

    Added configuration as a webpack alias

---
+++ configuration/default.js
@@ -0,0 +1,17 @@
+'use strict';
+
+let Configuration = {
+
+	lexicon: [
+		{
+			word: 'lorem',
+			media: 'http://...'
+		},
+		{
+			word: 'ipsum',
+			media: 'http://...'
+		}
+	]
+}
+
+module.exports = Configuration;
---
+++ configuration/production.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let Configuration = {
+
+	lexicon: [
+		{
+			word: 'temps',
+			media: 'http://...'
+		},
+		{
+			word: 'cela',
+			media: 'http://...'
+		},
+		{
+			word: 'mais',
+			media: 'http://...'
+		},
+		{
+			word: 'combien',
+			media: 'http://...'
+		},
+		{
+			word: 'helas',
+			media: 'http://...'
+		},
+		{
+			word: 'desormais',
+			media: 'http://...'
+		}
+	]
+}
+
+module.exports = Configuration;
