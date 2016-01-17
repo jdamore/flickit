088538a2e67a9e54e1b7b76d47966ba7c8e88c1e
Jean D'Amore <jdamore@thoughtworks.com>
2016-01-17 12:41:29 +1000

    Added random english words from wordnik to productIon lexicon

--- configuration/production.js
+++ configuration/production.js
@@ -4,29 +4,245 @@ let Configuration = {
 
 	lexicon: [
 		{
-			word: 'temps',
-			media: 'http://...'
-		},
-		{
-			word: 'cela',
-			media: 'http://...'
-		},
-		{
-			word: 'mais',
-			media: 'http://...'
-		},
-		{
-			word: 'combien',
-			media: 'http://...'
-		},
-		{
-			word: 'helas',
-			media: 'http://...'
-		},
-		{
-			word: 'desormais',
-			media: 'http://...'
-		}
+        "id": 95176,
+        "word": "arête"
+    },
+    {
+        "id": 435366,
+        "word": "epilobium"
+    },
+    {
+        "id": 70824,
+        "word": "metaphysical"
+    },
+    {
+        "id": 88125,
+        "word": "occasioning"
+    },
+    {
+        "id": 321381,
+        "word": "rassling"
+    },
+    {
+        "id": 210326,
+        "word": "subchondral"
+    },
+    {
+        "id": 7378471,
+        "word": "unfoldable"
+    },
+    {
+        "id": 225958,
+        "word": "unreason"
+    },
+    {
+        "id": 2721289,
+        "word": "unsupplemented"
+    },
+    {
+        "id": 228290,
+        "word": "vennel"
+    },
+    {
+        "id": 478296,
+        "word": "Butte"
+    },
+    {
+        "id": 1595329,
+        "word": "Caesareans"
+    },
+    {
+        "id": 611760,
+        "word": "Keepers"
+    },
+    {
+        "id": 32729,
+        "word": "cinerary"
+    },
+    {
+        "id": 257583,
+        "word": "commentaries"
+    },
+    {
+        "id": 70326,
+        "word": "fusible"
+    },
+    {
+        "id": 3558831,
+        "word": "lysophosphatidic"
+    },
+    {
+        "id": 2579692,
+        "word": "metacommunity"
+    },
+    {
+        "id": 91193,
+        "word": "rag-tag"
+    },
+    {
+        "id": 345740,
+        "word": "tremors"
+    },
+    {
+        "id": 28358098,
+        "word": "Moreton Bay"
+    },
+    {
+        "id": 556344,
+        "word": "Mortimer"
+    },
+    {
+        "id": 10144,
+        "word": "areolar"
+    },
+    {
+        "id": 2213080,
+        "word": "dabbawalas"
+    },
+    {
+        "id": 49814,
+        "word": "diplomatically"
+    },
+    {
+        "id": 124541,
+        "word": "leakiness"
+    },
+    {
+        "id": 191036,
+        "word": "river-dolphin"
+    },
+    {
+        "id": 339957,
+        "word": "supercities"
+    },
+    {
+        "id": 76131,
+        "word": "tester"
+    },
+    {
+        "id": 343991,
+        "word": "tigerlike"
+    },
+    {
+        "id": 640893,
+        "word": "Carrick"
+    },
+    {
+        "id": 243923,
+        "word": "audiocassette"
+    },
+    {
+        "id": 32967,
+        "word": "circumnavigator"
+    },
+    {
+        "id": 50944,
+        "word": "dispart"
+    },
+    {
+        "id": 7867191,
+        "word": "multiposition"
+    },
+    {
+        "id": 75163,
+        "word": "obliged"
+    },
+    {
+        "id": 312635,
+        "word": "perishables"
+    },
+    {
+        "id": 4391840,
+        "word": "supercells"
+    },
+    {
+        "id": 220091,
+        "word": "transpositive"
+    },
+    {
+        "id": 12398588,
+        "word": "universalisability"
+    },
+    {
+        "id": 489102,
+        "word": "Youngstown"
+    },
+    {
+        "id": 1934949,
+        "word": "appropriability"
+    },
+    {
+        "id": 28615,
+        "word": "centaury"
+    },
+    {
+        "id": 254634,
+        "word": "chicer"
+    },
+    {
+        "id": 255262,
+        "word": "chronobiology"
+    },
+    {
+        "id": 292845,
+        "word": "latecomers"
+    },
+    {
+        "id": 82849,
+        "word": "soften"
+    },
+    {
+        "id": 6333711,
+        "word": "steamie"
+    },
+    {
+        "id": 1349060,
+        "word": "westie"
+    },
+    {
+        "id": 83204,
+        "word": "widening"
+    },
+    {
+        "id": 484522,
+        "word": "Navajo"
+    },
+    {
+        "id": 2428,
+        "word": "adnexed"
+    },
+    {
+        "id": 23548,
+        "word": "burthenous"
+    },
+    {
+        "id": 263373,
+        "word": "defibrillator"
+    },
+    {
+        "id": 57234,
+        "word": "enchainment"
+    },
+    {
+        "id": 84701,
+        "word": "followers"
+    },
+    {
+        "id": 287123,
+        "word": "incentives"
+    },
+    {
+        "id": 180501,
+        "word": "precocity"
+    },
+    {
+        "id": 474952,
+        "word": "two-phase"
+    },
+    {
+        "id": 89014,
+        "word": "unparliamentary"
+    }
 	]
 }
 
