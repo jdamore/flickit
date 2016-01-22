849aff17ca508cbaef31ace44f84de75ad77f94c
Jean D'Amore <jdamore@thoughtworks.com>
2016-01-22 22:38:05 +1000

    Finished slaughtering jest

--- jest-preprocessor.js
+++
@@ -1,7 +0,0 @@
-var babelJest = require('babel-jest');
-
-module.exports = {
-    process: function(src, filename) {
-        return (filename.match(/\.[css|less|scss]/)) ? '' : babelJest.process(src, filename);
-    },
-};
