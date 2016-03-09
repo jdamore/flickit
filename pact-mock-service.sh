705221781f6227fc5f56c4102142748145f1d0da
Jean D'Amore <jdamore@thoughtworks.com>
2016-03-09 22:56:20 +1000

    More trace in pact-mock-service.sh

--- pact-mock-service.sh
+++ pact-mock-service.sh
@@ -18,26 +18,33 @@ setup() {
 
 start() 
 {
-	echo 'Starting Pact Mock Service...'
+	echo 'Downloading Pact Mock Service...'
 
 	if [ "$NODE_ENV" == "test" ]; then
 		if [ ! -d pact-mock-service-0.7.2-1-linux-x86_64 ]; then
 	  	curl -LO https://github.com/bethesque/pact-mock_service/releases/download/v0.7.2/pact-mock-service-0.7.2-1-linux-x86_64.tar.gz
 			tar xzf pact-mock-service-0.7.2-1-linux-x86_64.tar.gz
+			rm -f pact-mock-service-0.7.2-1-linux-x86_64.tar.gz
 		fi
-		cd pact-mock-service-0.7.2-1-linux-x86_64/bin
+		pushd pact-mock-service-0.7.2-1-linux-x86_64/bin
 	else
 		if [ ! -d pact-mock-service-0.7.2-1-osx ]; then
 			curl -LO https://github.com/bethesque/pact-mock_service/releases/download/v0.7.2/pact-mock-service-0.7.2-1-osx.tar.gz
 			tar xzf pact-mock-service-0.7.2-1-osx.tar.gz
+			rm -f pact-mock-service-0.7.2-1-osx.tar.gz
 		fi
-		cd pact-mock-service-0.7.2-1-osx/bin
+		pushd pact-mock-service-0.7.2-1-osx/bin
 	fi
 
+	echo 'Pact Mock Service Downloaded'
+
+
+	echo "Starting Pact Mock Service in $(pwd) for $(uname -a)..."
 	./pact-mock-service -p 1234 --pact-specification-version 2.0.0 -l $SCRIPT_DIR/pacts/pacts.log --pact-dir $SCRIPT_DIR/pacts &
 	echo $! > $SCRIPT_DIR/pact-mock-service.pid
-
 	echo 'Pact Mock Service Started'
+
+	popd
 
 }
 
