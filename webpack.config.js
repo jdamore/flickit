2b429aede0520724a128d6f6eefc8db2a3e5f810
Jean D'Amore <jdamore@thoughtworks.com>
2016-12-28 23:49:40 +0100

    Externalised CSS to style.css. Removed webpack prod config. Deleted lexicums in config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 var path = require('path');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var WatchIgnorePlugin = require('watch-ignore-webpack-plugin');
 
-module.exports = {
+var config = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
@@ -17,12 +19,9 @@ module.exports = {
                 query: {
                   presets: ['es2015', 'react']
                 }
-
             },
-
             // CSS Loader: style-loader, css-loader, sass-loader
-            { test: /\.scss$/, loaders: ['style', 'css', 'sass'], exclude: /node_modules/ },
-
+            { test: /\.scss$/, loader: ExtractTextPlugin.extract('style-loader', 'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!sass-loader') },
             // Other Loaders
             { test: /\.(png|jpg)$/, loader: 'file-loader?name=images/[name].[ext]' },
             { test: /\.woff$/, loader: 'file-loader?name=fonts/[name].[ext]' }
@@ -39,5 +38,18 @@ module.exports = {
         alias: {
             configuration: path.join(__dirname, 'configuration', 'default')
         }
-    }
-}
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css', {
+            allChunks: true
+        }),
+        new WatchIgnorePlugin([
+            path.resolve(__dirname, './node_modules/'),
+            path.resolve(__dirname, './images/'),
+            path.resolve(__dirname, './bundle.js'),
+            path.resolve(__dirname, './style.css')
+        ])
+    ]
+};
+
+module.exports = config;
