693f386857b5f5cab9f90ee69e7c8a8839fe6e57
Jean D'Amore <jdamore@thoughtworks.com>
2017-01-09 20:53:27 +1000

    Changed card behaviour to be only flippable once

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -26,7 +26,10 @@ export default class Card extends React.Component {
       <div id={'card-'+this.props.word} name='card' className={styles.card + ' ' + styles[this.state.value]} onClick={this.click.bind(this)}>
         <span id={'back-'+this.props.word} name='back' className={styles.back + ' ' + (this.state.value==='init' ? styles.shown : styles.hidden)}></span>
         <span id={'word-'+this.props.word} name='word' className={styles.word + ' ' + (this.state.value==='drawn' ? styles.shown : styles.hidden)}> {this.props.word} </span>
-        <span id={'score-'+this.props.word} name='score' className={styles.score + ' ' + (this.state.value==='flipped' ? styles.shown : styles.hidden)}> {this.props.score} </span>
+        <span id={'score-'+this.props.word} name='score' className={styles.score + ' ' + (this.state.value==='flipped' ? styles.shown : styles.hidden)}> 
+          <p>{this.props.word}</p>
+          <p>{this.props.score}</p> 
+        </span>
       </div>
     )
   }
